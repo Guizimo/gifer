4a24fc801430d542d75dce6263599121a5379534
Guizimo <17680262548@163.com>
2025-04-09 23:05:04 +0800

    refactor(scripts): 更新模块检查方式以支持ES Module

--- scripts/get-next-version.ts
+++ scripts/get-next-version.ts
@@ -1,12 +1,14 @@
 import { execSync } from 'child_process';
 import semver from 'semver';
+import { fileURLToPath } from 'url';
 
 export function getNextVersion(type: 'major' | 'minor' | 'patch'): string {
   const currentVersion = JSON.parse(execSync('npm pkg get version').toString().replace(/"/g, ''));
   return semver.inc(currentVersion, type) || currentVersion;
 }
 
-if (require.main === module) {
+// ES Module 方式检查是否直接运行
+if (import.meta.url === fileURLToPath(import.meta.url)) {
   const type = process.argv[2] as 'major' | 'minor' | 'patch';
   console.log(getNextVersion(type));
 }
